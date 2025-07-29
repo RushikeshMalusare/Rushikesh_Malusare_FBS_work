31fedec5488e2de124d9443947be889af1ae4f14
RushikeshMalusare <malusarerushikesh75@gmail.com>
2025-07-29 08:10:53 +0530

    Add files via upload

---
+++ Assignment/Assignent 1/character.c
@@ -0,0 +1,12 @@
+void main(){
+	char ch;
+	printf("enter the character\n");
+	scanf("%c",&ch);
+	if('a'==ch||'e'==ch||'i'==ch||'o'==ch||'u'==ch){
+		printf("character is vowels");
+	}
+	else{
+		printf("character is consonant");
+	}
+	
+}
