3a756fbefab0bfc7d7d362bbcf2ae42838da3e67
RushikeshMalusare <malusarerushikesh75@gmail.com>
2026-01-23 13:43:59 +0530

    Add files via upload

---
+++ corejava/6_october/src/ArrList.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ public class ArrList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet a1=new TreeSet();
+		a1.add(10);
+		a1.add(20);
+		a1.add(30);
+		a1.add(40);
+		System.out.println(a1);
+		System.out.println(a1);
+		//System.out.println(0);
+	}
+
+}
---
+++ corejava/6_october/src/Treeset.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Treeset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList a1=new ArrayList();
+				a1.add(10);
+				a1.add(20); 
+				a1.add(30);
+				a1.add(40);
+				System.out.println(a1);
+				//System.out.println(a1);
+				//System.out.println(0);
+			}
+
+		}
+
+	
+
+
